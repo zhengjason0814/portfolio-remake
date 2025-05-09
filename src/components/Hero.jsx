b643fccde0efa83238e24049b5b2f5f28d0b1e09
Jason Zheng <jasonzheng0814@gmail.com>
2025-05-09 03:42:34 -0400

    adjust web color to specific theme

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
             <div className={styles.main}>
                 <p>Hi, I am</p>
                 <h1><span className={styles.name}>Jason Zheng</span></h1>
-                <h2>I am a blah blah blah</h2>
+                <h2 className={styles.secondaryText}>I am a blah blah blah</h2>
             </div>
         </div>
     );
