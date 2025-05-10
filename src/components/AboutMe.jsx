fe34a2e93eb1823ebe3a10751b158cc4f70b280f
Jason Zheng <jasonzheng0814@gmail.com>
2025-05-10 03:40:04 -0400

    edit about me text

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -8,11 +8,11 @@ export default function AboutMe() {
             <div className={styles.textContainer}>
             <h1 style={{fontSize:"50px", color:"#B0B0B0"}}>About Me</h1>
             <p className={styles.aboutText}>
-                Based in NY 🗽, I'm a student working towards a B.S. in Information Systems at Stony Brook University. Throughout my academics, I've developed a skillset with programming languages, web development, and database management technologies through projects.
+                Based in NY 🗽, I'm a student working towards a B.S. in Information Systems at Stony Brook University. I've developed skillsets with programming languages, web development, and database management technologies.
                 <br /><br />
-                In a past internship, I have also had the opportunity to not only be a part of but lead a new project that utilized OpenAI's API to develop new technologies to streamline user inquiries about their lease sales website.
+                In a past internship, I have also had the opportunity to not only be a part of but lead a new project that utilized API's to develop new technologies to streamline user inquiries about their lease sales website.
                 <br /><br />
-                If there are any questions you may have about me, please feel free to reach out to me at jasonzheng0814@gmail.com!
+                If there are any questions you may have about me, please feel free to reach out!
             </p>
         </div>
         <img className={styles.waffle} src={waffle}/>
