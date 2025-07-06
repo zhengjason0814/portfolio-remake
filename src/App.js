9db493558d05af4d3045d9109d481bef77cadcee
Jason Zheng <jasonzheng0814@gmail.com>
2025-07-06 01:01:11 -0400

    scrap MUI

--- src/App.js
+++ src/App.js
@@ -4,40 +4,18 @@ import Header from './components/Header';
 import AboutMe from './components/AboutMe';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
-import { Container } from '@mui/material';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#90caf9'
-    },
-    secondary: {
-      main: '#f48fb1'
-    },
-    background: {
-      default: '#121212'
-  },
-  },
-  typography: {
-    fontFamily:'Poppins, Arial, sans-serif'
-  }
-});
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Container>
+    <div className="container">
       <Header />
       <Hero />
       <AboutMe />
       <Experience />
       <Projects />
-      </Container>
-    </ThemeProvider>
+      <Contact />
+    </div>
   );
 }
 
